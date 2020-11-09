3dbda971eaadb65dedd9a1517b1c577096d9c12b
Gabriel Agrela <sopustos@gmail.com>
2020-11-09 02:43:02 +0000

    -Sheet with 2 earning release days of companies; -Api calls for each company set up, aswell as the client side wait to match the api's free rate; -Stock price evolution and its graph of earning releases "after close" (TODO: before open)

---
+++ EarningReleasesStock.py
@@ -0,0 +1,70 @@
+#TODO: expand sheet; code the "before open" companies
+
+import urllib.request, json
+import datetime
+from datetime import timedelta
+import sheet,time
+import matplotlib.pyplot as plt
+import numpy as np
+
+stockEvolutions = []
+key=""
+sheet.main()
+
+#get percentage (up/down) from 2 days diff (deppending on stock market closing or opening)
+def calculateStockEvolution(stockPriceBefore,stockPriceAfter):
+	return 100 - (stockPriceAfter*100/stockPriceBefore)
+
+def stock(company,x):
+	global stockEvolutions
+	stockPriceBefore=0
+	stockPriceAfter=0
+
+	REDate = sheet.companiesAfterClose[x][0] #date in sheet
+	REDate = datetime.datetime.strptime(REDate, '%y-%m-%d').date() #format string to date
+
+	#api call to get symbol from company in sheet cell
+	with urllib.request.urlopen("https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords="+company.replace(' ', '%20')+"&apikey="+key) as url:
+		data = json.loads(url.read().decode())
+		try:
+			company=str(data['bestMatches'][0]["1. symbol"])
+		except:
+			print(str(data['Note']))
+			quit()
+
+	print("symbol: "+company)
+	time.sleep(13)
+
+	#api call to get stock price after close, and the price on open of the day after
+	with urllib.request.urlopen("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol="+company+"&outputsize=compact&apikey="+key) as url:
+		data = json.loads(url.read().decode())
+		try:
+			print("REDate close stock price:" + str(data['Time Series (Daily)'][REDate.strftime("%Y-%m-%d")]['4. close']))
+			stockPriceBefore=float(data['Time Series (Daily)'][REDate.strftime("%Y-%m-%d")]['4. close'])
+			print("REDate+1day open stock price:" + str(data['Time Series (Daily)'][(REDate  + timedelta(days=1)).strftime("%Y-%m-%d")]['1. open']))
+			stockPriceAfter=float(data['Time Series (Daily)'][(REDate  + timedelta(days=1)).strftime("%Y-%m-%d")]['1. open'])
+			print("Stock evolution: "+str("{:.2f}".format(calculateStockEvolution(stockPriceBefore,stockPriceAfter)))+"% \n")
+			stockEvolutions.append(float("{:.2f}".format(calculateStockEvolution(stockPriceBefore,stockPriceAfter))))
+		except:
+			print(str(data['Note']))
+			quit()
+
+	time.sleep(13) #(5 call per minute allowed, each company needs 2 calls, 60/13<5)
+
+
+#companiesAfterClose 2d list divided by day
+for x in range(0,len(sheet.companiesAfterClose)):
+	for i in range(1,len(sheet.companiesAfterClose[x])):
+		print("\n"+sheet.companiesAfterClose[x][i]+":")
+		stock(sheet.companiesAfterClose[x][i],x)
+
+#how did this stock(company, x)index perform
+totalStockEvolution=0
+for j in range(0,len(stockEvolutions)):
+	totalStockEvolution = totalStockEvolution + float(stockEvolutions[j])
+print(totalStockEvolution)
+
+#graph stock evolution of each comapny
+plt.plot(stockEvolutions,"o")
+plt.xlabel('Stock Evolution')
+plt.show()
